31225804491da7d6b1c1c0f9b3a2412dfbecb57c
rkakbk83 <87198087+rkakbk83@users.noreply.github.com>
2022-06-30 19:34:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$.ajax({
+    type :'get',
+    url : "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian",
+    success : function(response)
+    {
+        console.log(response.meals)
+        for( var i=0;i<response.meals.length;i++)
+        {
+            var newitem=`<div class="col-md-3 text-center shadow-lg p-3 mb-5 bg-white rounded" m-2">
+            <p> ${response.meals[i].strMeal}<p/>
+            <img src="${response.meals[i].strMealThumb}" class="img-fluid"/>
+            <p>${response.meals[i].idMeal}</p>
+            </div>`
+            $('#myitem').append(newitem)
+        }
+    },
+    error : function(error)
+    {
+        console.log(error)
+    }
+})
